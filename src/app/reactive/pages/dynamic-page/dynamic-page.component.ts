647c8bd699092c753f0a0a347e608a8ca6e71e55
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-19 13:44:53 +0200

    validaciones dinámicas

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -24,6 +24,36 @@ export class DynamicPageComponent {
     return this.myForm.get('favoriteGames') as FormArray;
   }
 
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  isValidFieldInArray(formArray: FormArray, i: number) {
+    return (
+      formArray.controls[i].errors && formArray.controls[i].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlegth':
+          return `Mínimo ${errors['minlegth'].requiredLength} caracters.`;
+      }
+    }
+
+    return null;
+  }
+
+
   onSubmit(): void {
     if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
