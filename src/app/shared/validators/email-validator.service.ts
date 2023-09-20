3535f3fdfed036de39ac2d3bd46bdac1af8c52c3
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-20 09:46:42 +0200

    elaborar validador de correo

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -11,13 +11,33 @@ export class EmailValidator implements AsyncValidator {
   //tiene que es una promesa que devuelva un ValidationErrors o null o un Observable que devuelva ValidationErrors o null
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
     const email = control.value;
+
+    const httpCallObservable = new Observable<ValidationErrors | null>(
+      (subscriber) => {
+        console.log({ email });
+
+        if (email === 'raquel@gmail.com') {
+          subscriber.next({ emailTaken: true });
+          subscriber.complete();
+          //return
+        }
+        //si no es el correo anterior, NULL
+        subscriber.next(null);
+        subscriber.complete();
+      }
+    ).pipe(delay(3000));
+    return httpCallObservable;
+  }
+
+  /* validate(control: AbstractControl): Observable<ValidationErrors | null> {
+    const email = control.value;
     console.log({ email });
 
-    //forma de regresar un Observable
+    forma de regresar un Observable
     return of({
       emailTaken: true,
     }).pipe(
         delay(2000)
     )
-  }
+  } */
 }
